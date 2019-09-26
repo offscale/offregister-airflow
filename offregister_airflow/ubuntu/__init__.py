a5f3954820480761606775a333696425713b45f9
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-09-27 01:04:27 +1000

    Improve environment handling

--- offregister_airflow/ubuntu/__init__.py
+++ offregister_airflow/ubuntu/__init__.py
@@ -5,23 +5,26 @@ import offregister_python.ubuntu as python_mod
 
 
 def install0(*args, **kwargs):
-    venv0_kwargs = {
-        'virtual_env': '/opt/venvs/airflow',
-        'python3': True,
-        'pip_version': '19.2.3',
-        'use_sudo': True,
-        'remote_user': 'ubuntu',
-        'PACKAGES': [
-            'apache-airflow[postgres,redis]'
-        ]
-    }
-    venv0_kwargs.update(kwargs)
-    python_mod.install_venv0(**venv0_kwargs)
+    kwargs.setdefault('virtual_env', '/opt/venvs/airflow')
+
+    if not kwargs.get('skip_virtualenv', False):
+        venv0_kwargs = {
+            'virtual_env': kwargs['virtual_env'],
+            'python3': True,
+            'pip_version': '19.2.3',
+            'use_sudo': True,
+            'remote_user': 'ubuntu',
+            'PACKAGES': [
+                'apache-airflow[postgres,redis]'
+            ]
+        }
+        venv0_kwargs.update(kwargs)
+        python_mod.install_venv0(**venv0_kwargs)
 
     circus0_kwargs = {
         'APP_NAME': 'airflow',
         'APP_PORT': 8045,
-        'CMD': '{virtual_env}/bin/airflow'.format(virtual_env=venv0_kwargs['virtual_env']),
+        'CMD': '{virtual_env}/bin/airflow'.format(virtual_env=kwargs['virtual_env']),
         'CMD_ARGS': 'webserver',
         'WSGI_FILE': None
     }
