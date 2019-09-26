abe05b5ed467de72182058036481118c056c19fe
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-09-26 21:50:46 +1000

    Init

---
+++ offregister_airflow/ubuntu/__init__.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+
+import offregister_circus.ubuntu as circus_mod
+import offregister_python.ubuntu as python_mod
+
+
+def install0(*args, **kwargs):
+    venv0_kwargs = {
+        'virtual_env': '/opt/venvs/airflow',
+        'python3': True,
+        'pip_version': '19.2.3',
+        'use_sudo': True,
+        'remote_user': 'ubuntu',
+        'PACKAGES': [
+            'apache-airflow[postgres,redis]'
+        ]
+    }
+    venv0_kwargs.update(kwargs)
+    python_mod.install_venv0(**venv0_kwargs)
+
+    circus0_kwargs = {
+        'APP_NAME': 'airflow',
+        'APP_PORT': 8045,
+        'CMD': '{virtual_env}/bin/airflow'.format(virtual_env=venv0_kwargs['virtual_env']),
+        'CMD_ARGS': 'webserver',
+        'WSGI_FILE': None
+    }
+    circus0_kwargs.update(kwargs)
+    circus_mod.install_circus0(**circus0_kwargs)
