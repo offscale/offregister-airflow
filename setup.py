31d88d2e8090dc7c211b164538ed4e0cf4f95a84
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-12-30 23:08:10 -0500

    [setup.py,__init__.py] Standardise install scripts across all off* Python packages; [LICENSE-{APACHE,MIT}] Update copyright year range

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from ast import parse
-from functools import partial
-from os import listdir, path
-import sys
+"""
+setup.py implementation, interesting because it parsed the first __init__.py and
+    extracts the `__author__` and `__version__`
+"""
 
+from ast import Assign, Name, parse
+from functools import partial
+from operator import attrgetter
+from os import listdir, path
+from os.path import extsep
+import sys
 from setuptools import find_packages, setup
 
 if sys.version_info[:2] >= (3, 12):
@@ -74,42 +80,106 @@ if sys.version_info[:2] >= (3, 12):
                 "I don't know where Python installs its library "
                 "on platform '%s'" % os.name
             )
-
+    from ast import Del as Str
 else:
     from distutils.sysconfig import get_python_lib
+    from ast import Str
 
 if sys.version_info[0] == 2:
     from itertools import ifilter as filter
     from itertools import imap as map
 
-if __name__ == "__main__":
-    package_name = "offregister_airflow"
+if sys.version_info[:2] > (3, 7):
+    from ast import Constant
+else:
+    from ast import expr
 
-    with open(path.join(package_name, "__init__.py")) as f:
-        __author__, __version__ = imap(
-            lambda buf: next(imap(lambda e: e.value.s, parse(buf).body)),
-            ifilter(
-                lambda line: line.startswith("__version__")
-                or line.startswith("__author__"),
-                f,
-            ),
-        )
+    # Constant. Will never be used in Python =< 3.8
+    Constant = type("Constant", (expr,), {})
 
-    to_funcs = lambda *paths: (
+
+package_name_verbatim = "offregister_airflow"
+package_name = package_name_verbatim.replace("-", "_")
+
+with open(
+    path.join(path.dirname(__file__), "README{extsep}md".format(extsep=extsep)),
+    "rt",
+) as fh:
+    long_description = fh.read()
+
+
+def to_funcs(*paths):
+    """
+    Produce function tuples that produce the local and install dir, respectively.
+
+    :param paths: one or more str, referring to relative folder names
+    :type paths: ```*paths```
+
+    :return: 2 functions
+    :rtype: ```Tuple[Callable[Optional[List[str]], str], Callable[Optional[List[str]], str]]```
+    """
+    return (
         partial(path.join, path.dirname(__file__), package_name, *paths),
         partial(path.join, get_python_lib(prefix=""), package_name, *paths),
     )
+
+
+def main():
+    """Main function for setup.py; this actually does the installation"""
+    with open(
+        path.join(
+            path.abspath(path.dirname(__file__)),
+            package_name,
+            "__init__{extsep}py".format(extsep=extsep),
+        )
+    ) as f:
+        parsed_init = parse(f.read())
+
+    __author__, __version__, __description__ = map(
+        lambda node: node.value if isinstance(node, Constant) else node.s,
+        filter(
+            lambda node: isinstance(node, (Constant, Str)),
+            map(
+                attrgetter("value"),
+                filter(
+                    lambda node: isinstance(node, Assign)
+                    and any(
+                        filter(
+                            lambda name: isinstance(name, Name) and name.id
+                            in frozenset(
+                                ("__author__", "__version__", "__description__")
+                            ),
+                            node.targets,
+                        )
+                    ),
+                    parsed_init.body,
+                ),
+            ),
+        ),
+    )
+
     _data_join, _data_install_dir = to_funcs("_data")
 
     setup(
-        name=package_name,
+        name=package_name_verbatim,
         author=__author__,
         version=__version__,
+        description=__description__,
         install_requires=["pyyaml"],
         test_suite=package_name + ".tests",
         packages=find_packages(),
         package_dir={package_name: package_name},
         data_files=[
-            (_data_install_dir(), list(imap(_data_join, listdir(_data_join()))))
+            (_data_install_dir(), list(map(_data_join, listdir(_data_join()))))
         ],
     )
+
+
+
+def setup_py_main():
+    """Calls main if `__name__ == '__main__'`"""
+    if __name__ == "__main__":
+        main()
+
+
+setup_py_main()
